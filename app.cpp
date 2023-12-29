f6675a16de36976770919a53ee37d17b15fc232e
Thomas Leber <herschdorfer@gmail.com>
2023-12-29 16:54:15 +0100

    test

--- app.cpp
+++ app.cpp
@@ -3,9 +3,25 @@
 #include <stdexcept>
 
 extern int buffer_overflow();
+extern int com_inj();
+extern int cata();
+extern int string();
+extern int leak();
+extern int int_over();
+extern int priv();
+extern int race();
+extern int update();
 
 int main() {
   buffer_overflow();
+  com_inj();
+  cata();
+  string();
+  leak();
+  int_over();
+  priv();
+  race();
+  update();
 
   return 0;
 }
